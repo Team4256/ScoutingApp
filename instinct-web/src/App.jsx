a1d323ead465d82a5635dc635326dafa0f189156
fairyninjaCM <fairyninjacm@gmail.com>
2026-06-16 18:34:21 -0500

    app setup

--- instinct-web/src/App.jsx
+++ instinct-web/src/App.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <h1>4256 Scouting</h1>
+
+      <button
+        type="button"
+        className="counter"
+        onClick={() => setCount((count) => count + 1)}
+      >
+        Points: {count}
+      </button>
     </>
   );
 }
