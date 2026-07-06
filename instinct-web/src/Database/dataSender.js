d24a71620475e5e39c0290b4e0814ff72769c0fb
Lucas <LucT5@icloud.com>
2026-07-06 16:03:48 -0500

    http json sending works

--- instinct-web/src/Database/dataSender.js
+++ instinct-web/src/Database/dataSender.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function sendDataToServer(data) {
   try {
-    const response = await axios.post("https://webhook.site/2a34cf08-7553-4633-9cc3-750ac0bf1efa", data);
+    const response = await axios.post("http://localhost:3000/match", data);
     console.log("Data sent successfully:", response.data);
   } catch (error) {
     console.error("Error sending data:", error);
